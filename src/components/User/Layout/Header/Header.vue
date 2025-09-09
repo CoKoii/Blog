7f133e29ac8166e50ce7c3bded66a62daea95539
CK <2655257336@qq.com>
2025-09-09 13:48:22 +0800

    优化 Header 组件，添加搜索按钮及样式，调整布局和遮罩层样式

--- src/components/User/Layout/Header/Header.vue
+++ src/components/User/Layout/Header/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { VIcon } from 'void-design-vue'
 import { inject, type Ref } from 'vue'
 const LayoutRef = inject<Ref<HTMLElement>>('layoutRef')
@@ -16,9 +16,14 @@ const onMenuClick = () => {
       </span>
       <RouterLink to="/">Cheng</RouterLink>
     </div>
-
+    <button class="search">
+      <VIcon :icon="faSearch" size="sm" />
+      Search
+      <span>/</span>
+    </button>
     <div class="tools"></div>
   </div>
+  <div class="mask"></div>
 </template>
 
 <style scoped lang="scss">
