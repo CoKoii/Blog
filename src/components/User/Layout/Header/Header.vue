2ed67c0261b16fe4566d1bffa091c33d9e5edd43
CK <2655257336@qq.com>
2025-09-09 14:03:03 +0800

    优化 Header 组件，添加搜索功能及样式，调整遮罩层样式和布局

--- src/components/User/Layout/Header/Header.vue
+++ src/components/User/Layout/Header/Header.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { VIcon } from 'void-design-vue'
-import { inject, type Ref } from 'vue'
+import { inject, ref, type Ref } from 'vue'
 const LayoutRef = inject<Ref<HTMLElement>>('layoutRef')
 const onMenuClick = () => {
   LayoutRef?.value.classList.toggle('open')
+}
+const showSearch = ref(false)
+const openSearch = () => {
+  showSearch.value = true
+  document.body.style.overflow = 'hidden'
+}
+const closeSearch = () => {
+  showSearch.value = false
+  document.body.style.overflow = 'auto'
 }
 </script>
 
@@ -16,14 +25,18 @@ const onMenuClick = () => {
       </span>
       <RouterLink to="/">Cheng</RouterLink>
     </div>
-    <button class="search">
+    <button class="search" @click="openSearch">
       <VIcon :icon="faSearch" size="sm" />
       Search
       <span>/</span>
     </button>
     <div class="tools"></div>
   </div>
-  <div class="mask"></div>
+  <teleport to="body">
+    <div class="mask" v-if="showSearch" @click="closeSearch">
+      <div class="search"></div>
+    </div>
+  </teleport>
 </template>
 
 <style scoped lang="scss">
