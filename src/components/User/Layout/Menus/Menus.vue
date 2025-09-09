163fab7b2e4c1d4a7577fcf9e2c0553491345d98
CK <2655257336@qq.com>
2025-09-09 21:54:10 +0800

    优化菜单组件，添加更多功能和标签链接；调整样式以改善布局和用户体验

--- src/components/User/Layout/Menus/Menus.vue
+++ src/components/User/Layout/Menus/Menus.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { faAddressCard, faFolderOpen, faHome } from '@fortawesome/free-regular-svg-icons'
+import { faAddressCard, faCompass, faFolderOpen, faHome } from '@fortawesome/free-regular-svg-icons'
 import { VIcon } from 'void-design-vue'
 </script>
 
 <template>
   <div class="Menus">
+    <!-- 主页面菜单 -->
     <ul class="menus_main">
       <li>
         <router-link to="/home" active-class="active">
@@ -22,6 +23,41 @@ import { VIcon } from 'void-design-vue'
         <router-link to="/about" active-class="active">
           <VIcon :icon="faAddressCard" />
           About</router-link
+        >
+      </li>
+    </ul>
+    <div class="line"></div>
+    <!-- 更多功能 -->
+    <h1 class="title">More Features</h1>
+    <ul class="menus_main">
+      <li>
+        <router-link to="/more" active-class="active">
+          <VIcon :icon="faCompass" />
+          正在发掘...</router-link
+        >
+      </li>
+    </ul>
+    <!-- 标签 -->
+    <h1 class="title">Tags</h1>
+    <ul class="menus_main">
+      <li>
+        <router-link to="/vuejs" active-class="active">
+          <span class="dots" style="background-color: #ff5f57"></span>
+          VueJs</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/javascript" active-class="active">
+          <span class="dots" style="background-color: #febc2e"></span>
+
+          JavaScript</router-link
+        >
+      </li>
+      <li>
+        <router-link to="/nodejs" active-class="active">
+          <span class="dots" style="background-color: #28c840"></span>
+
+          Node.js</router-link
         >
       </li>
     </ul>
