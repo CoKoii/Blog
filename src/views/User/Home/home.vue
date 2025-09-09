2723cacb54e05abd529cb8f55406d15f8f22dcbc
CK <2655257336@qq.com>
2025-09-09 22:05:41 +0800

    更新首页内容，移除多余的标题，保留简洁的展示

--- src/views/User/Home/home.vue
+++ src/views/User/Home/home.vue
@@ -2,54 +2,6 @@
 
 <template>
   <div class="home">
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus vitae facilis eum rem
-      repellat, accusantium assumenda itaque recusandae voluptates non sapiente reiciendis neque
-      rerum, hic sequi necessitatibus voluptatem delectus pariatur.
-    </h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
-    <h1>我是首页</h1>
     <h1>我是首页2</h1>
   </div>
 </template>
