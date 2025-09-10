04dd37d34d68e7cbcc855286785526058534efa2
CK <2655257336@qq.com>
2025-09-10 12:52:50 +0800

    重构菜单组件，使用 v-for 动态生成菜单项；优化样式变量以提升可维护性和一致性

--- src/components/User/Layout/Menus/Menus.vue
+++ src/components/User/Layout/Menus/Menus.vue
@@ -1,63 +1,64 @@
 <script setup lang="ts">
 import { faAddressCard, faCompass, faFolderOpen, faHome } from '@fortawesome/free-regular-svg-icons'
 import { VIcon } from 'void-design-vue'
+
+type MenuItem = {
+  to: string
+  label: string
+  icon?: unknown
+}
+
+type TagItem = {
+  to: string
+  label: string
+  color: string
+}
+
+const mainMenus: MenuItem[] = [
+  { to: '/home', label: 'Home', icon: faHome },
+  { to: '/archive', label: 'Archive', icon: faFolderOpen },
+  { to: '/about', label: 'About', icon: faAddressCard },
+]
+
+const moreMenus: MenuItem[] = [{ to: '/more', label: '正在发掘...', icon: faCompass }]
+
+const tags: TagItem[] = [
+  { to: '/vuejs', label: 'VueJs', color: '#ff5f57' },
+  { to: '/javascript', label: 'JavaScript', color: '#febc2e' },
+  { to: '/nodejs', label: 'Node.js', color: '#28c840' },
+]
 </script>
 
 <template>
   <div class="Menus">
     <!-- 主页面菜单 -->
     <ul class="menus_main">
-      <li>
-        <router-link to="/home" active-class="active">
-          <VIcon :icon="faHome" />
-          <span>Home</span></router-link
-        >
-      </li>
-      <li>
-        <router-link to="/archive" active-class="active">
-          <VIcon :icon="faFolderOpen" />
-          <span>Archive</span></router-link
-        >
-      </li>
-      <li>
-        <router-link to="/about" active-class="active">
-          <VIcon :icon="faAddressCard" />
-          <span>About</span></router-link
-        >
+      <li v-for="item in mainMenus" :key="item.to">
+        <router-link :to="item.to" active-class="active">
+          <VIcon v-if="item.icon" :icon="item.icon" />
+          <span>{{ item.label }}</span>
+        </router-link>
       </li>
     </ul>
     <div class="line"></div>
     <!-- 更多功能 -->
     <h1 class="title">More Features</h1>
     <ul class="menus_main">
-      <li>
-        <router-link to="/more" active-class="active">
-          <VIcon :icon="faCompass" />
-          <span>正在发掘...</span></router-link
-        >
+      <li v-for="item in moreMenus" :key="item.to">
+        <router-link :to="item.to" active-class="active">
+          <VIcon v-if="item.icon" :icon="item.icon" />
+          <span>{{ item.label }}</span>
+        </router-link>
       </li>
     </ul>
     <!-- 标签 -->
     <h1 class="title">Tags</h1>
     <ul class="menus_main">
-      <li>
-        <router-link to="/vuejs" active-class="active">
-          <span class="dots" style="background-color: #ff5f57"></span>
-          <span>VueJs</span></router-link
-        >
-      </li>
-      <li>
-        <router-link to="/javascript" active-class="active">
-          <span class="dots" style="background-color: #febc2e"></span>
-
-          <span>JavaScript</span></router-link
-        >
-      </li>
-      <li>
-        <router-link to="/nodejs" active-class="active">
-          <span class="dots" style="background-color: #28c840"></span>
-          <span>Node.js</span></router-link
-        >
+      <li v-for="tag in tags" :key="tag.to">
+        <router-link :to="tag.to" active-class="active">
+          <span class="dots" :style="{ backgroundColor: tag.color }"></span>
+          <span>{{ tag.label }}</span>
+        </router-link>
       </li>
     </ul>
   </div>
