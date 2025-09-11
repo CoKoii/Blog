937393ddfe05c036959cccabb0b4fde1f09c7e33
CK <2655257336@qq.com>
2025-09-11 13:09:55 +0800

    优化 Archive 组件，添加 Tags 和 Articles 组件，调整样式以改善布局

--- src/views/User/Archive/Archive.vue
+++ src/views/User/Archive/Archive.vue
@@ -1,7 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Articles from '@/components/User/Articles/Articles.vue'
+import Tags from '@/components/User/Tags/Tags.vue'
+</script>
 
 <template>
-  <div class="Archive">Archive</div>
+  <div class="Archive">
+    <Tags />
+    <Articles />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.Archive {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+</style>
