25c05d6e64cac371b3402c5dbfe0df536ea507af
CK <2655257336@qq.com>
2025-09-11 19:49:42 +0800

    重构 Tags 组件，移除点击计数功能，优化模板结构，确保标签信息和文章组件正确渲染

--- src/views/User/Tags/Tags.vue
+++ src/views/User/Tags/Tags.vue
@@ -1,11 +1,70 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const count = ref(0)
+import Articles from '@/components/User/Articles/Articles.vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const tag = route.params.id
 </script>
 
 <template>
-  <div class="Tags" @click="count++">{{ count }}</div>
+  <div class="Tags">
+    <div class="info">
+      <div class="text">
+        <div class="num">4 posts</div>
+        <div class="title">{{ tag }} <span></span></div>
+        <div class="desc">
+          JavaScript是一个广泛使用的编程语言。他的语法简洁，功能强大，广泛应用于Web开发。
+        </div>
+      </div>
+      <img
+        loading="lazy"
+        src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=1200&auto=format&fit=crop"
+        alt=""
+      />
+    </div>
+    <Articles mode="infinite" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.Tags {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  .info {
+    display: flex;
+    align-items: center;
+    gap: 64px;
+    .text {
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+      .num {
+        font-size: 14px;
+      }
+      .title {
+        font-size: 45px;
+        font-weight: 600;
+        letter-spacing: 2px;
+        display: flex;
+        align-items: center;
+        gap: 12px;
+        span {
+          display: inline-block;
+          width: 16px;
+          height: 16px;
+          background-color: #ff5f57;
+          border-radius: 50%;
+        }
+      }
+      .desc {
+        font-size: 16px;
+      }
+    }
+  }
+  img {
+    width: 50%;
+    aspect-ratio: 16/9;
+    border-radius: 20px;
+  }
+}
+</style>
